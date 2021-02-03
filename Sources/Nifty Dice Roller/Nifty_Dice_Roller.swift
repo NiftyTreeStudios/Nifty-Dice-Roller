31d2ff9b8f7067bb795ba2b9d3b5d47cefb421c9
iiroalhonen <iiro.alhonen@kpsbremen.de>
2021-02-03 12:50:40 +0100

    Documentation for the basic function.

--- Sources/Nifty Dice Roller/Nifty_Dice_Roller.swift
+++ Sources/Nifty Dice Roller/Nifty_Dice_Roller.swift
@@ -1,7 +1,19 @@
-struct Nifty_Dice_Roller {
-    var text = "Hello, World!"
-}
+//
+//  Nifty-Dice-Roller.swift
+//  Nifty Dice Roller
+//
+//  Created by Iiro Alhonen on 3.2.2021.
+//  Copyright © 2021 Nifty Tree Studios. All rights reserved.
+//
 
+/**
+ This function is used to roll the dice and returning the total of the roll.
+ 
+ - Author: Iiro Alhonen
+ - Parameter dice: The dice used
+ - Parameter diceAmount: The amount of dice being rolled
+ - Parameter modifier: The modifier used for the roll (-5, +6, etc.)
+ */
 func roll(dice: Int, diceAmount: Int, modifier: Int?) -> Int {
     let modifier = modifier ?? 0
     return diceAmount * dice + modifier
