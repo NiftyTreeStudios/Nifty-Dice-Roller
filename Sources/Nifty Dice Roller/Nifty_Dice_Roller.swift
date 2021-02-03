b2eb57cb1e8e52c0a672d37e34fce13c22af3d07
iiroalhonen <iiro.alhonen@kpsbremen.de>
2021-02-03 11:56:58 +0100

    Initial commit

--- Sources/Nifty Dice Roller/Nifty_Dice_Roller.swift
+++ Sources/Nifty Dice Roller/Nifty_Dice_Roller.swift
@@ -1,3 +1,8 @@
 struct Nifty_Dice_Roller {
     var text = "Hello, World!"
 }
+
+func roll(dice: Int, diceAmount: Int, modifier: Int?) -> Int {
+    let modifier = modifier ?? 0
+    return diceAmount * dice + modifier
+}
