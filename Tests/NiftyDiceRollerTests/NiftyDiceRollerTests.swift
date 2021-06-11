9bd3350ae320c991963f0048031a76966dbb5727
iiroalhonen <iiro.alhonen@kpsbremen.de>
2021-06-11 11:09:06 +0200

    Added tests for individual dice rolls.

--- Tests/NiftyDiceRollerTests/NiftyDiceRollerTests.swift
+++ Tests/NiftyDiceRollerTests/NiftyDiceRollerTests.swift
@@ -2,13 +2,17 @@ import XCTest
 @testable import NiftyDiceRoller
 
 final class NiftyDiceRollerTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+    func testDiceRolls() {
+        XCTAssertTrue(diceRoll(dice: .d4) <= 4)
+        XCTAssertTrue(diceRoll(dice: .d6) <= 6)
+        XCTAssertTrue(diceRoll(dice: .d8) <= 8)
+        XCTAssertTrue(diceRoll(dice: .d10) <= 10)
+        XCTAssertTrue(diceRoll(dice: .d12) <= 12)
+        XCTAssertTrue(diceRoll(dice: .d20) <= 20)
+        XCTAssertTrue(diceRoll(dice: .d100) <= 100)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testExample", testDiceRolls),
     ]
 }
